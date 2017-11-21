b4677e240bdbcce6edb55b161d47ff64fa031237
Sainath Latkar <latkarsainath@gmail.com>
2017-11-21 18:47:02 +0530

    make android app working again

--- android/app/src/main/java/com/yohoho/MainApplication.java
+++ android/app/src/main/java/com/yohoho/MainApplication.java
@@ -9,11 +9,21 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -24,9 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new FBSDKPackage()
+              new MainReactPackage(),
+              new FBSDKPackage(mCallbackManager)
       );
     }
   };
@@ -39,6 +48,8 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
   }
 }
--- index.android.js
+++ index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,42 +6,151 @@ import {
   View
 } from 'react-native';
 
-export default class Yohoho extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+
+import { connect,Provider } from 'react-redux';
+import { Router ,Actions, Scene, TabBar} from 'react-native-router-flux';
+import { createStore, applyMiddleware, combineReduxers, compose }from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+
+import reducer from './app/reducers'
+import AppContainer from './app/containers/AppContainer'
+import Icon from 'react-native-vector-icons/FontAwesome';
+/*
+ * Applying logger middleware only in dev. __DEV__ is some global variable in nodejs*/
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__  });
+import TabIconFeed from './app/containers/components/TabIconFeed'
+import TabIconSearch from './app/containers/components/TabIconSearch'
+import TabIconProfile from './app/containers/components/TabIconProfile'
+
+import SignInPage from './app/containers/influencer/SignInPage';
+import SignUpPage from './app/containers/influencer/SignUpPage';
+import UpdateInfoPage from './app/containers/influencer/UpdateInfoPage';
+import NicheSelectPage from './app/containers/influencer/NicheSelectPage';
+import FeedPage from './app/containers/influencer/FeedPage';
+import ProfilePage from './app/containers/influencer/ProfilePage';
+import SearchPage from './app/containers/influencer/SearchPage';
+import chatPage from './app/containers/influencer/chatPage';
+import chatPage2 from './app/containers/influencer/chatPage2';
+import chatListPage from './app/containers/influencer/chatListPage';
+import InfluencersListPage from './app/containers/influencer/InfluencersListPage';
+import BrandsListPage from './app/containers/influencer/BrandsListPage';
+import PendingRequestsPage from './app/containers/influencer/PendingRequestsPage';
+import VisitProfilePage from './app/containers/influencer/VisitProfilePage';
+import ActionCableChatPage from './app/containers/influencer/ChatClassActionCable';
+/*Use the following scene to test redux functionality*/
+import Test from './app/containers/Test'
+
+/*
+ * For seting up react native flux router
+ * boilerplate
+ */
+const ConnectedRouter = connect()(Router);
+//Style for TabBar
+let style = StyleSheet.create({
+    tabBarStyle: {
+            // flex : 1,
+            borderTopWidth : 0.5,
+            borderColor    : '#8887b2',
+            backgroundColor: '#FFF',
+            opacity        : 1
+        }
+    });
+
+//Flux Scenes
+/*PUT SCENES HERE
+ * put all the scenes here.
+ * use proper component names
+ * */
+const scenes = Actions.create(
+    <Scene {...this.props} key="root">
+
+        <Scene key="SignInPage" component={Test}
+            hideNavBar = {true}
+            initial = {true}
+        />
+        <Scene key="SignUpPage" component={SignUpPage} hideNavBar />
+        <Scene key="SignInPage" component={SignInPage} hideNavBar />
+        <Scene key="UpdateInfoPage" component={UpdateInfoPage} hideNavBar/>
+        <Scene key="NicheSelectPage" component={NicheSelectPage} hideNavBar />
+        <Scene key="chatPage" component={chatPage} hideNavBar/>
+        <Scene key="chatPage2" component={chatPage2} hideNavBar/>
+        <Scene key="InfluencersListPage" component={InfluencersListPage} hideNavBar/>
+        <Scene key="PendingRequestsPage" component={PendingRequestsPage} hideNavBar/>
+        <Scene key="BrandsListPage" component={BrandsListPage} hideNavBar/>
+        <Scene key="chatListPage" component={chatListPage} hideNavBar/>
+        <Scene key="VisitProfilePage" component={VisitProfilePage} hideNavBar/>
+        <Scene key="ActionCableChatPage" component={ActionCableChatPage} hideNavBar/>
+        <Scene key="tabbar" tabs={true} showLabel={false} swipeEnabled tabBarStyle={style.tabBarStyle}  >
+            <Scene key="home" component={FeedPage} title={"Feed"} icon={TabIconFeed} iconName={"home"} hideNavBar />
+            <Scene key="SearchPage" component={SearchPage} title={"Search"} icon={TabIconSearch} iconName={"search"} hideNavBar />
+            <Scene key="ProfilePage" component={ProfilePage} title={"Profile"} icon={TabIconProfile} iconName={"user"} hideNavBar />
+        </Scene>
+
+    </Scene>
+
+);
+
+//Redux Store
+/*CONFIGURING STORE
+ * Configuring store with two middlewares
+ * loggleMiddleware prints state with action in google chrome
+ * boilerplate;
+ * may be moved to a different file according to the need
+ * */
+function configureStore(initialState){
+    const enhancer = compose(
+        applyMiddleware(
+            thunkMiddleware,
+            loggerMiddleware,
+        ),
     );
-  }
+    return createStore(reducer, initialState, enhancer);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+/*
+ * If you want to have some initial state for the app put it in configurestore's parameter.
+ * inside {}
+ * */
+const store = configureStore({
+  profileData : {
+    niche : [],
+    contact: {},
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  feedData : {
+    businesses : [],
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  signedInUser:{
+    data : {
+    },
+    headers : {
+
+    }
   },
+  chatOBJ:{
+
+  },
+  messageObj:{
+
+  }
+
 });
+
+//Main APP
+/*MAIN APP
+ * Boilerplate.
+ * react native flux router | redux
+ * */
+ export default class Yohoho extends Component {
+   render(){
+     return(
+     <Provider store = {store}>
+  <ConnectedRouter scenes={scenes} />
+     </Provider>
+   )
+ }
+ };
+
+
 
 AppRegistry.registerComponent('Yohoho', () => Yohoho);

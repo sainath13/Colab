9ad217f8e5fb84f5bbd90c6a57a940b017738b1f
Sainath Latkar <latkarsainath@gmail.com>
2018-04-29 15:59:13 +0530

    bug fix

--- app/containers/influencer/VisitProfilePage.js
+++ app/containers/influencer/VisitProfilePage.js
@@ -739,7 +739,7 @@ class VisitProfilePage extends Component {
                                                 }}>
                                                     <Text style={styles.informationSlotCardPricingText}>
                                                         {!this.state.fetching
-                                                            ? this.props.visitProfileData.price_per_story + this.props.visitProfileData.currency
+                                                            ? this.props.visitProfileData.price_per_post + this.props.visitProfileData.currency
                                                             : null}
                                                     </Text>
                                                 </View>
@@ -755,7 +755,7 @@ class VisitProfilePage extends Component {
                                                 }}>
                                                     <Text style={styles.informationSlotCardPricingText}>
                                                         {!this.state.fetching
-                                                            ? this.props.visitProfileData.price_per_post +  this.props.visitProfileData.currency
+                                                            ? this.props.visitProfileData.price_per_story +  this.props.visitProfileData.currency
                                                             : null}
                                                     </Text>
                                                 </View>
